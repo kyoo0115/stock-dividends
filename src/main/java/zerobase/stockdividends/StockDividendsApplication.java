4be0cc77808fa7be6dae846851e539347d77ea5a
kyoo0115 <internetkyoomin@gmail.com>
2024-04-02 14:39:40 +0800

    feat: implemented scraping result to list

--- src/main/java/zerobase/stockdividends/StockDividendsApplication.java
+++ src/main/java/zerobase/stockdividends/StockDividendsApplication.java
@@ -2,11 +2,19 @@ package zerobase.stockdividends;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zerobase.stockdividends.model.Company;
+import zerobase.stockdividends.scraper.YahooFinanceScraper;
 
 @SpringBootApplication
 public class StockDividendsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StockDividendsApplication.class, args);
+//        SpringApplication.run(StockDividendsApplication.class, args);
+        YahooFinanceScraper scraper = new YahooFinanceScraper();
+        var result = scraper.scrap(Company.builder()
+                .ticker("COKE")
+                .build());
+
+        System.out.println(result);
     }
 }
--- src/main/java/zerobase/stockdividends/scraper/YahooFinanceScraper.java
+++ src/main/java/zerobase/stockdividends/scraper/YahooFinanceScraper.java
@@ -1,11 +1,72 @@
 package zerobase.stockdividends.scraper;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import zerobase.stockdividends.model.Company;
+import zerobase.stockdividends.model.Dividend;
 import zerobase.stockdividends.model.ScrapedResult;
+import zerobase.stockdividends.model.constants.Month;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class YahooFinanceScraper {
 
-    public ScrapedResult scrape(Company company) {
+    private static final String STATISTICS_URL = "https://finance.yahoo.com/quote/%s/history?period1=%d&period2=%d&interval=1mo";
+    private static final long START_TIME = 86400;
+
+    public ScrapedResult scrap(Company company) {
+        var scrapedResult = new ScrapedResult();
+        scrapedResult.setCompany(company);
+
+        try{
+            long now = System.currentTimeMillis() / 1000;
+
+            String url = String.format(STATISTICS_URL, company.getTicker(), START_TIME, now);
+            Connection connection = Jsoup.connect(url);
+            Document document = connection.get();
+
+            Elements parsingDivs = document.getElementsByAttributeValue("data-test", "historical-prices");
+            Element tableElement = parsingDivs.get(0);
+
+            Element tbody = tableElement.children().get(1);
+
+            List<Dividend> dividends = new ArrayList<>();
+
+            for (Element e : tbody.children()) {
+                String text = e.text();
+                if(!text.endsWith("Dividend")) continue;
+
+                String[] splits = text.split(" ");
+                int month = Month.strToNumber(splits[0]);
+                int day = Integer.parseInt(splits[1].replace(",", ""));
+                int year = Integer.parseInt(splits[2]);
+                String dividend = splits[3];
+
+                if(month < 0) {
+                    throw new RuntimeException("Unexpected Month enum value -> " + splits[0]);
+                }
+
+                dividends.add(Dividend.builder()
+                        .date(LocalDateTime.of(year, month, day, 0, 0))
+                        .dividend(dividend)
+                        .build());
+
+            }
+            scrapedResult.setDividendEntities(dividends);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return scrapedResult;
+    }
+
+    public Company scrapCompanyByTicker(String ticker) {
         return null;
     }
 }
